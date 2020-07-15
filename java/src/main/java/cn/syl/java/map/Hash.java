6e68e8693c6cd8d918ead817340130ec1b12a50a
suiyiliang <1311150301@qq.com>
2020-07-15 19:12:18 +0800

    11

---
+++ java/src/main/java/cn/syl/java/map/Hash.java
@@ -0,0 +1,10 @@
+package cn.syl.java.map;
+
+import java.util.HashMap;
+
+public class Hash {
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<>();
+        System.out.println(map.get("23131"));
+    }
+}
---
+++ java/src/main/java/cn/syl/java/tree/rb/Node.java
@@ -0,0 +1,10 @@
+package cn.syl.java.tree.rb;
+
+public class Node{
+    private int data;
+    private Node left;
+    private Node right;
+    private Node parent;
+    private boolean red;
+
+}
