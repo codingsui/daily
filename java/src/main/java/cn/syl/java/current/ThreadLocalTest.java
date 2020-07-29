52fc5ca5a3f0fb29e323ba4147770979a6f8bae4
suidada <suidada@192.168.0.105>
2020-07-29 09:10:11 +0800

    14

---
+++ java/src/main/java/cn/syl/java/current/ThreadLocalTest.java
@@ -0,0 +1,42 @@
+package cn.syl.java.current;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadLocalTest {
+    private static ThreadLocal<SimpleDateFormat> sdf = new ThreadLocal<>();
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        for (int i = 0; i < 11; i++) {
+            executorService.submit(new DateUtil("2019-11-25 09:00:" + i % 60));
+        }
+
+    }
+
+
+    static class DateUtil implements Runnable{
+        private String date;
+
+        public DateUtil(String date) {
+            this.date = date;
+        }
+        @Override
+        public void run() {
+            if (sdf.get() == null){
+                sdf.set(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            }else{
+                Date date = null;
+                try {
+                    date = sdf.get().parse(this.date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(date);
+            }
+        }
+    }
+
+}
