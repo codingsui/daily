6bd2bf1c07f17139b56cbef7c51335199c324d44
suiyiliang <1311150301@qq.com>
2020-09-11 18:49:41 +0800

    最近好忙

--- java/src/main/java/cn/syl/java/current/InterruptTest.java
+++ java/src/main/java/cn/syl/java/current/InterruptTest.java
@@ -25,6 +25,13 @@ public class InterruptTest {
         System.out.println("main==========="+Thread.interrupted());
         System.out.println("main==========="+Thread.interrupted());
         System.out.println("main==========="+Thread.interrupted());
+
+        System.out.println("test3======");
+        Thread thread = new Thread(()->{
+            System.out.println("=="+Thread.interrupted());
+        }); //定义一个线程，伪代码没有具体实现
+        System.out.println(Thread.interrupted());
+        System.out.println(Thread.interrupted());
     }
 
 }
